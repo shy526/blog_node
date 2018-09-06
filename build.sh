d46c80b615f81ead9cab7e07ab604d46980cf0d5
sunjiaqing <sundada214@outlook.com>
2018-09-06 17:46:18 +0800

    修复一个空格问题

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 #BUILD_ID=DONTKILLME
 APP_NAME="blog_node"
 # 移动至目标目录
-RUN_PATH="/home/project/node/blog/blog_node/"
-RUN_PATH_PARENT="/home/project/node/blog/"
+RUN_PATH="/home/project/node/blog_node/"
 
  log(){
     echo `date "+%Y-%m-%d %H:%M:%S  ----[$1]----$APP_NAME:$2"`
@@ -42,15 +41,10 @@ info_log "$APP_NAME"
 if [ ! $RUN_PATH ];then
   error_log "is null"
 fi
-# 没有该文件夹 旧重新拉取
-if [ ! -d "$RUN_PATH" ];then
-     info_log "克隆仓库"
-    git clone https://github.com/sunjiaqing/blog_node.git "$RUN_PATH_PARENT"
-fi
 
 
 info_log "拉取新文件"
-`GIT_DIR=/home/project/node/blog/blog_node/.git git pull`
+git pull
 info_log "下载mod"
 npm install
 
